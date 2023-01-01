4462fdda5a8752cca51b4d5633ada60c15a75461
Riley Mahr <rwmahr@gmail.com>
2023-01-01 12:30:27 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import GcodeViewer from './Renderer/Gcode';
+import FileUpload from './File/FileUpload';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App FlexContainer">
+      <div className="Left">
+        <FileUpload></FileUpload>
+      </div>
+      <div className="Right">
+        <GcodeViewer></GcodeViewer>
+      </div>
     </div>
   );
 }
---
+++ src/File/FileUpload.js
@@ -0,0 +1,27 @@
+import {useState} from 'react'
+
+function FileUpload(props) {
+
+    const [metadata, setMetadata] = useState()
+    const [gcode, setGcode] = useState()
+
+    const showFile = async (e) => {
+        e.preventDefault()
+        const reader = new FileReader()
+        reader.onload = async (e) => { 
+          const text = (e.target.result)
+          console.log(text)
+          alert(text)
+        };
+        reader.readAsText(e.target.files[0])
+      }
+
+    return (
+        <form>
+            <h1>Input Gcode</h1>
+            <input type="file" onChange={(e) => showFile(e)}/>
+        </form>
+    )
+}
+
+export default FileUpload
